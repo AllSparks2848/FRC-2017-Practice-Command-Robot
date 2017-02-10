c6e72bda7779811edb030dfeeaaad6fa7aa7c7d1
Zero2848 <18187@jcpstudents.org>
2017-02-10 11:13:34 -0600

    Added robot state class, will be used once we have encoders in

--- src/org/usfirst/frc/team2852/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team2852/robot/subsystems/DriveTrain.java
@@ -2,11 +2,16 @@ package org.usfirst.frc.team2852.robot.subsystems;
 
 import org.usfirst.frc.team2852.driveCommands.ArcadeDrive;
 import org.usfirst.frc.team2852.robot.RobotMap;
+import org.usfirst.team2852.robot.util.Rotation2d;
+
+import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveTrain extends PIDSubsystem {
 	public Spark leftDrive1 = new Spark(RobotMap.p_leftDrive1);
@@ -15,6 +20,7 @@ public class DriveTrain extends PIDSubsystem {
 	public Spark rightDrive1 = new Spark(RobotMap.p_rightDrive1);
 	public Spark rightDrive2 = new Spark(RobotMap.p_rightDrive2);
 	public Spark rightDrive3 = new Spark(RobotMap.p_rightDrive3);
+	public AHRS gyro=new AHRS(SPI.Port.kMXP);
 	
 	RobotDrive drive1 = new RobotDrive(leftDrive1, leftDrive2, rightDrive1, rightDrive2);
 	RobotDrive drive2 = new RobotDrive(leftDrive3, rightDrive3);
@@ -26,6 +32,29 @@ public class DriveTrain extends PIDSubsystem {
 	private static double pDrive = .07;
 	private static double iDrive = 0;
 	private static double dDrive = .007;
+	 private int leftEncoderZero, rightEncoderZero;
+	    private boolean leftReverseEnc = false;
+	    private boolean rightReverseEnc = true;
+	    private int leftEncSign = 1;
+	    private int rightEncSign = -1;
+	    
+	    private boolean isHighGear = false;
+	   
+	    
+	    public double kPHigh = 0, kIHigh = 0, kDHigh = 0;
+	    public double kPLow = 0, kILow = 0, kDLow = 0;
+	    public double kP = kPLow, kI = kILow, kD = kDLow;
+	    public double kFLowLeft = 1.316, kFLowRight = 1.340;
+	    public double kFHighLeft = 0.456, kFHighRight = 0.465;
+		public double kFLeft = kFLowLeft, kFRight = kFLowRight;
+		double pPos = 0, iPos = 0, fPos = 0;
+		int iZone = 0;
+		
+		private static final double WHEEL_DIAMETER_IN = 3.95; // Measured
+		private static final int COUNTS_PER_REV = 360; // Quadrature counts = 1440
+		
+		double leftSetpoint, rightSetpoint;
+		double tolerance = 4.0 / COUNTS_PER_REV;
 	
 //	public static Encoder rightEncoder = new Encoder(RobotMap.p_rightEncoderA, RobotMap.p_rightEncoderB, false, Encoder.EncodingType.k4X);
 //	public static Encoder leftEncoder = new Encoder(RobotMap.p_leftEncoderA, RobotMap.p_leftEncoderB, false, Encoder.EncodingType.k4X);
@@ -92,5 +121,153 @@ public class DriveTrain extends PIDSubsystem {
     public void shiftLow() {
     	driveShifter.set(DoubleSolenoid.Value.kReverse);
     }
+
+	/*
+	public void stop() {
+		setMotorsRaw(0,0);
+	}
+	
+	// Converts inches/second to motor percentage using feed-forward term
+	public void setMotorsInchesPerSecondOpenLoop(double left, double right) {
+		double leftSpeed = kFLeft * inchesPerSecondToRPM(left) * COUNTS_PER_REV / 1023 / 60;
+		double rightSpeed = kFRight * inchesPerSecondToRPM(right) * COUNTS_PER_REV / 1023 / 60;
+		
+		setMotors(leftSpeed, rightSpeed);
+	}
+	
+	public void setMotors(double left, double right) {
+		left = scaleLeft(left);
+		right = scaleRight(right);
+		
+		setMotorsRaw(left, right);
+	}
+
+	public void setMotorsRaw(double left, double right) {
+		left = safetyTest(left);
+		right = safetyTest(right);
+		
+		leftDrive1.set(left);
+		leftDrive2.set(left);
+		leftDrive3.set(left);
+		rightDrive1.set(right);
+		rightDrive2.set(right);
+		rightDrive3.set(right);
+				
+	}
+	
+	private double safetyTest(double motorValue) {
+	    motorValue = (motorValue < -1) ? -1 : motorValue;
+	    motorValue = (motorValue > 1) ? 1 : motorValue;
+	    
+	    return motorValue;
+	}
+	*/
+	
+/*
+	
+	
+	
+	public boolean nearSetpoint() {
+		double currentLeftPosition = leftMotorFront.getPosition();
+		boolean nearLeft = Math.abs(leftSetpoint - currentLeftPosition) < tolerance;
+		
+		double currentRightPosition = rightMotorFront.getPosition();
+		boolean nearRight = Math.abs(rightSetpoint - currentRightPosition) < tolerance;
+		
+		return nearLeft && nearRight;
+	}
+	
+	private double scaleLeft(double left) {
+		return left;
+	}
+	
+	private double scaleRight(double right) {
+		return right;
+	}
+	
+	public void resetEncoders() {
+		leftEncoderZero = leftEncMotor.getEncPosition();
+		rightEncoderZero = rightEncMotor.getEncPosition();
+	}
+	
+	public int getLeftEncoder() {
+		return leftEncSign * (leftEncMotor.getEncPosition() - leftEncoderZero);
+	}
+	
+	public int getRightEncoder() {
+		return rightEncSign * (rightEncMotor.getEncPosition() - rightEncoderZero);
+	}
+	
+	public double getLeftPositionInches() {
+		double rotations = ((double) getLeftEncoder()) / (4 * COUNTS_PER_REV);
+		
+		return rotationsToInches(rotations);
+	}
+	
+	public double getRightPositionInches() {
+		double rotations = ((double) getRightEncoder()) / (4 * COUNTS_PER_REV);
+		
+		return rotationsToInches(rotations);
+	}
+	
+	public double getLeftVelocity() {
+		return leftEncMotor.getSpeed();
+	} 
+	
+	public double getRightVelocity() {
+		return rightEncMotor.getSpeed();
+	}
+	
+	public double getLeftVelocityInchesPerSecond() {
+		return rpmToInchesPerSecond(getLeftVelocity());
+	}
+	
+	public double getRightVelocityInchesPerSecond() {
+		return rpmToInchesPerSecond(getRightVelocity());
+	}
+	
+	public boolean isLeftEncPresent() {
+		return !(leftEncMotor.isSensorPresent(FeedbackDevice.QuadEncoder) == FeedbackDeviceStatus.FeedbackStatusPresent);
+	}
+	
+	public boolean isRightEncPresent() {
+		return !(rightEncMotor.isSensorPresent(FeedbackDevice.QuadEncoder) == FeedbackDeviceStatus.FeedbackStatusPresent);
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	private double rotationsToInches(double rotations) {
+		return rotations * (Math.PI * WHEEL_DIAMETER_IN);
+	}
+	
+	private double inchesToRotations(double inches) {
+		return inches / (Math.PI * WHEEL_DIAMETER_IN);
+	}
+	
+	private double rpmToInchesPerSecond(double rpm) {
+		return rotationsToInches(rpm) / 60;
+	}
+	
+	private double inchesPerSecondToRPM(double inchesPerSecond) {
+		return inchesToRotations(inchesPerSecond * 60);
+	}*/
+	public Rotation2d getGyro() {
+		SmartDashboard.putData("IMU", gyro);
+		
+		return Rotation2d.fromDegrees(gyro.getAngle());
+	}
+	
+	public void resetGyro() {
+		gyro.reset();
+	}
 }
 
